d0ad9a895bf962dbeaf7a98efb056af1ab5f7c47
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-02-26 17:04:05 -0500

    Add init add users page skeleton

---
+++ xpartan/View/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  xpartan
+//
+//  Created by Cristian Sánchez Pineda on 25/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        NavigationStack {
+            
+            ScrollView {
+                VStack(spacing: 20){
+                    EmptyUsersView()
+                }
+                .navigationTitle("Users")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Button(action: {true}){
+                            Image(systemName: "plus.circle.fill")
+                                .font(.title2)
+                                .foregroundColor(.red.opacity(0.7))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+    
+}
+
+struct EmptyUsersView: View {
+    var body: some View {
+        
+        VStack(spacing: 16) {
+            Image(systemName: "figure.highintensity.intervaltraining.circle")
+                .font(.system(size:60))
+                .foregroundColor(.red.opacity(0.6))
+            
+            
+            Text("No users yet")
+                .font(.title2)
+                .bold()
+            
+            Text("Tap + to add a new Best")
+                .foregroundColor(.secondary)
+        }
+        .padding(.top, 200)
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
